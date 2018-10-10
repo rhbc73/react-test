462bcd991527ba0a95daae76821e0819b3d58c2d
Unknown <rhbc73@gmail.com>
2018-10-11 00:09:27 +1100

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React, { Component } from 'react';
-import './App.css';
-import Person from './Person/Person'
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      persons:[
-        { name : "xuanyu", age : "35"},
-        { name : "yiran",age : "27"}]
-    }
+      persons: [{ name: "xuanyu", age: "35" }, { name: "yiran", age: "27" }],
+      toshow: true
+    };
   }
 
   // switchnamehandler = (idx) => {
@@ -20,24 +19,37 @@ class App extends Component {
   // }
 
   onnamechange = (event, idx) => {
-    console.log(event.target.value)
-    const dup = this.state.persons.slice()
-    dup[idx].name = event.target.value
+    console.log(event.target.value);
+    const dup = this.state.persons.slice();
+    dup[idx].name = event.target.value;
 
-    this.setState({persons:dup})
-  }
+    this.setState({ persons: dup });
+  };
 
-  render() {
+  togglehide = () => {
+    this.setState({ toshow: !this.state.toshow });
+  };
+
+  render = () => {
+    const action = this.state.toshow ? "Hide all" : "Show all";
+    let content = null;
+    if (this.state.toshow)
+      content = this.state.persons.map((el, idx) => (
+        <Person
+          name={el.name}
+          age={el.age}
+          onnamechange={event => this.onnamechange(event, idx)}
+        />
+      ));
+
     return (
       <div className="App">
         <h3>Hi, I'm a React app</h3>
-        <button onClick={this.switchnamehandler}>switch</button>
-        {this.state.persons.map((el, idx) => <Person name={el.name} age={el.age}
-        onnamechange={(event) => this.onnamechange(event, idx)}
-        />)}
+        <button onClick={this.togglehide}>{action}</button>
+        {content}
       </div>
     );
-  }
+  };
 }
 
 export default App;
